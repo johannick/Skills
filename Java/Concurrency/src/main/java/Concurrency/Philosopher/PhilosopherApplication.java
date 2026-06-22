6fc27a5676ccfc51c391f794cebb3992a5117445
johannick <johannick.renaud@archange.solutions>
2026-06-22 20:35:13 +0200

    [SKILLS] Add Java Folder

---
+++ Java/Concurrency/src/main/java/Concurrency/Philosopher/PhilosopherApplication.java
@@ -0,0 +1,13 @@
+package Concurrency.Philosopher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosopherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilosopherApplication.class, args);
+	}
+
+}
---
+++ Java/Concurrency/src/test/java/Concurrency/Philosopher/PhilosopherApplicationTests.java
@@ -0,0 +1,13 @@
+package Concurrency.Philosopher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosopherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
